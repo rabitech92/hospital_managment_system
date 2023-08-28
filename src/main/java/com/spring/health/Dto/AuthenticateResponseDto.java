ab4ae5e7c52f4cd2698e13f72dd745c14c2c266b
rabiul islam <rabiulnewsinfo@gmail.com>
2023-08-28 08:20:43 +0600

    JWT Authentication

---
+++ src/main/java/com/spring/health/Dto/AuthenticateResponseDto.java
@@ -0,0 +1,16 @@
+package com.spring.health.Dto;
+
+public class AuthenticateResponseDto implements Serializable {
+
+    private static final long serialVersionUID = -8091879091924046844L;
+    private final String jwttoken;
+    public AuthenticateResponseDto(String jwttoken) {
+        this.jwttoken = jwttoken;
+    }
+
+    public String getToken() {
+        return this.jwttoken;
+    }
+
+
+}
