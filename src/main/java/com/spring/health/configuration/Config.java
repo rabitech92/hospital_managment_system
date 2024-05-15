9cf4606986f64943f1319e171a51343d21fdc6bd
rabiul islam <rabiulnewsinfo@gmail.com>
2024-05-15 15:06:19 +0600

    add componenetScan

--- src/main/java/com/spring/health/configuration/Config.java
+++ src/main/java/com/spring/health/configuration/Config.java
@@ -2,9 +2,11 @@ package com.spring.health.configuration;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "com.spring.health")
 public class Config {
 
     @Bean
