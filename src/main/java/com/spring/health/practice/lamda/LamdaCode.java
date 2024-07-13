03b72892d8fb8186a9e634208a4e540c4f4851c2
rabiul islam <rabiulnewsinfo@gmail.com>
2024-07-13 16:57:11 +0600

    [office] stream done

---
+++ src/main/java/com/spring/health/practice/lamda/LamdaCode.java
@@ -0,0 +1,47 @@
+package com.spring.health.practice.lamda;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class LamdaCode {
+
+    public static void main(String[] args) {
+        //mapping
+        List<Integer> integerList = Arrays.asList(1,2,3,4,5,6,7,8,9);
+        List<Integer> list = integerList.stream()
+                .map(n ->n*n)
+                .collect(Collectors.toList());
+        System.out.println(list);
+
+
+        List<String> stringList = Arrays.asList("Jamal","Kamal","Hasib","Rahman","Astreammamal","Kamal","Hasib","ARahman");
+        List<String> strings = stringList.stream()
+                .filter(s -> s.equalsIgnoreCase("hasib")).collect(Collectors.toList());
+        System.out.println(strings);
+
+
+        List<String>  sorting = Arrays.asList("Abdullah","Jama","Kamal","Rahim","Afifa");
+        List<String> sorted = sorting.stream()
+                .sorted()
+                .collect(Collectors.toList());
+        System.out.println(sorted);
+
+        integerList.stream()
+                .forEach(num -> System.out.print(num+","));
+
+       List<String> strings1 = strings.stream()
+                .collect(Collectors.toList());
+        System.out.println(strings1);
+
+        List<String> strings2 = strings.stream()
+                .filter(name ->name.startsWith("A"))
+                .map(name -> name.toUpperCase())
+                .sorted()
+                .collect(Collectors.toList());
+        System.out.println(strings2);
+    }
+
+}
