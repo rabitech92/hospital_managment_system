b77461ad3ed35a89318c376926326b914df0cc67
rabiul islam <rabiulnewsinfo@gmail.com>
2024-07-30 10:11:31 +0600

    [office] list update fully

--- src/main/java/com/spring/health/practice/streamList/MainClass.java
+++
@@ -1,5 +0,0 @@
-package com.spring.health.practice.streamList;
-
-public class MainClass {
-
-}
--- src/main/java/com/spring/health/practice/streamList/Service.java
+++ src/main/java/com/spring/health/practice/streamList/Service.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class Service {
@@ -17,10 +18,30 @@ public class Service {
 
         List<Guardian> guardians = new ArrayList<>();
         guardians.add(guardian);
-        System.out.println(guardians);
+//        System.out.println(guardians);
+
+        Teacher teacher = new Teacher();
+        teacher.setId(202);
+        teacher.setName("Taiamur rahman");
+
+       List<Teacher> teachers = new ArrayList<>();
+        teachers.add(teacher);
+
+        Student student = new Student();
+        student.setId(303);
+        student.setName("Kawser Ahmed");
+        student.setTeacher(teachers);
+        student.setGuardians(guardians);
+
+
+        List<Student> students = new ArrayList<>();
+        students.add(student);
 
 
 
+        List<Student> list = students.stream()
+                .collect(Collectors.toList());
+        System.out.println(list);
 
     }
 }
