8e750e54df2819d69f821315e44ba7ed30529d5e
rabiul islam <rabiulnewsinfo@gmail.com>
2024-09-29 17:22:05 +0600

    string

---
+++ src/main/java/com/spring/health/practice/string/StringCode.java
@@ -0,0 +1,10 @@
+package com.spring.health.practice.string;
+
+public class StringCode {
+    public static void main(String[] args) {
+        String str = "Hello";
+        String str1 = new String("Hello");
+
+        System.out.println(str == str1);
+    }
+}
